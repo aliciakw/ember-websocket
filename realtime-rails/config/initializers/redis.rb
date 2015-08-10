f6e74032049c2c5f4e621918913749098c4db9ae
aliciakw <alicia.willett@gmail.com>
2015-08-10 15:31:07 -0400

    publish db changes to websocket channel via callbacks to redis

--- realtime-rails/config/initializers/redis.rb
+++ realtime-rails/config/initializers/redis.rb
@@ -1,7 +1,5 @@
-# puts '############################'
-# puts 'Launching Redis server....'
-# puts '############################'
-$redis = Redis.new(:host => 'localhost', :port=> 6379)
-# location = ENV["REDISCLOUD_URL"] || 'redis://127.0.0.1:6379/0'
-# uri = URI.parse(location)
-# $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+ puts 'Launching Redis server....'
+
+location = ENV["REDISCLOUD_URL"] || 'redis://127.0.0.1:6379/0'
+uri = URI.parse(location)
+$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
--- realtime-rails/realtime/realtime-server.js
+++ realtime-rails/realtime/realtime-server.js
@@ -1,10 +1,11 @@
-var io = require('socket.io').listen(5001),
-    redis = require('redis').createClient();
-
+var io = require('socket.io').listen(5001);
+var redis = require('redis').createClient();
 redis.subscribe('rt-change');
+console.log('SERVER STARTED at PORT 5001');
 
 io.on('connection', function(socket){
   redis.on('message', function(channel, message){
-    socket.emit('rt-change', JSON.parse(message));
+    console.log(message); // this sends the data but there's something weird with the object
+    socket.emit('rt-change', JSON.parse(message)); // where is this emitted?
   });
 });
