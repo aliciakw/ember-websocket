72ac72f7b4003cbe01d0215013a658f101488f7b
aliciakw <alicia.willett@gmail.com>
2015-08-13 15:00:30 -0400

    seeds, merge conflicts

--- realtime-rails/db/seeds.rb
+++ realtime-rails/db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Book.create(title:"On Beauty", author:"Zadie Smith", num_pages: 445)
 Book.create(title:"Taipei", author:"Tao Lin", num_pages: 248)
+Book.create(title:"On Beauty", author:"Zadie Smith", num_pages: 445)
 Book.create(title:"Fun Home", author:"Alison Bechdel", num_pages: 234)
+Book.create(title:"100 Years of Solitude", author:"Gabriel Garcia Marquez", num_pages: 457)
+Book.create(title:"Threats", author: "Amelia Gray", num_pages:278)
